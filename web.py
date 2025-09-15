734b82b957094b48389f3bacb479d2266be56fc5
Simon Fonteneau <sfonteneau@tranquil.it>
2025-09-15 13:48:34 +0200

    backup csr and cert

--- web.py
+++ web.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, Response, g
+import os
 import uuid
 import base64
 import configparser
+import textwrap
+import hashlib
 import xml.etree.ElementTree as ET
 from datetime import datetime, timedelta
 from jinja2 import Template
@@ -67,26 +70,36 @@ def ces_service():
 
     csr_der, body_part_id, info = exct_csr_from_cmc(p7_der)
 
+    sha256_hex = hashlib.sha256(csr_der).hexdigest()
+
+    with open(os.path.join(app.confadcs['path_csr'],str(sha256_hex) + '.pem'),'w') as f :
+        f.write("-----BEGIN CERTIFICATE REQUEST-----\n" + "\n".join(textwrap.wrap(format_b64_for_soap(csr_der), 64)) + "\n-----END CERTIFICATE REQUEST-----")
+
     ca_cert_pem = base64.b64decode(app.confadcs['ca_certificate'])
     ca_key_pem  = base64.b64decode(app.confadcs['ca_pem'])
 
     samdbr,entry = search_user(g.kerberos_user)
     sid = samdbr.schema_format_value("objectSID", entry["objectSID"][0])
-
+    sr = cx509.random_serial_number()
     leaf = issue_cert(csr_der, 
                       ca_cert_pem, ca_key_pem,cn=g.kerberos_user,
                       crl_http_url = app.confadcs['crl_http_url'],
                       ca_issuers_http_url = app.confadcs['ca_issuers_http_url'],
                       sid_bytes = sid,
-                      info_csr=info)
+                      info_csr=info,
+                      sr=sr)
 
     leaf_der = leaf.public_bytes(serialization.Encoding.DER)
 
     p7b_der = build_adcs_bst_certrep(leaf_der, ca_cert_pem, app.confadcs['ca_key_load'], body_part_id)
+    b64_leaf=format_b64_for_soap(p7b_der)
+
+    with open(os.path.join(app.confadcs['path_cert'],str(sr) + '.pem'),'w') as f :
+        f.write("-----BEGIN CERTIFICATE-----\n" + "\n".join(textwrap.wrap(b64_leaf, 64)) + "\n-----END CERTIFICATE-----")
 
     response_xml = app.confadcs['tpl_adcs_ca_ces_Kerberos_service_svc_CES'].render(    uuid_request=uuid_request,
                                   uuid_random=str(uuid.uuid4()),
-                                  p7b_der=format_b64_for_soap(p7b_der),
+                                  p7b_der=b64_leaf,
                                   leaf_der = format_b64_for_soap(leaf_der),
                                   body_part_id=body_part_id
                               )
@@ -94,16 +107,17 @@ def ces_service():
 
 
     return Response(response_xml, content_type='application/soap+xml')
-
-def issue_cert(csr_der: bytes, ca_cert_der: bytes, ca_key_der: bytes, cn: str, crl_http_url: str,ca_issuers_http_url: str,sid_bytes: bytes, info_csr=None, entry_ad = None) -> cx509.Certificate:
+def issue_cert(csr_der: bytes, ca_cert_der: bytes, ca_key_der: bytes, cn: str, crl_http_url: str,ca_issuers_http_url: str,sid_bytes: bytes, info_csr=None, entry_ad = None, sr = None) -> cx509.Certificate:
     csr_obj  = cx509.load_der_x509_csr(csr_der)
     ca_cert  = cx509.load_der_x509_certificate(ca_cert_der)
     ca_key   = serialization.load_der_private_key(ca_key_der, password=None)
     now = datetime.utcnow() - timedelta(minutes=5)
 
-    subject = csr_obj.subject if csr_obj.subject.rdns else cx509.Name([
-        cx509.NameAttribute(NameOID.COMMON_NAME, cn )
-    ])
+    #subject = csr_obj.subject if csr_obj.subject.rdns else cx509.Name([
+    #    cx509.NameAttribute(NameOID.COMMON_NAME, cn )
+    #])
+
+    subject = cx509.Name([cx509.NameAttribute(NameOID.COMMON_NAME, cn )])
 
     san_ext = None
     eku_ext = None
@@ -177,13 +191,14 @@ def issue_cert(csr_der: bytes, ca_cert_der: bytes, ca_key_der: bytes, cn: str, c
         "attr_values": [a_core.OctetString(sid_bytes)],
     })
     ntds_der = NtdsCASecurityExt([ntds_attr]).dump()
-
+    if not sr :
+        sr = cx509.random_serial_number()
     builder = (
         cx509.CertificateBuilder()
         .subject_name(subject)
         .issuer_name(ca_cert.subject)
         .public_key(csr_obj.public_key())
-        .serial_number(cx509.random_serial_number())
+        .serial_number(sr)
         .not_valid_before(now)
         .not_valid_after(now + timedelta(days=365))
         .add_extension(ku_ext, critical=True)
@@ -217,6 +232,8 @@ if __name__ == '__main__':
     confadcs['crl_http_url']        = config.get("global", "crl_http_url")
     confadcs['ca_issuers_http_url'] = config.get("global", "ca_issuers_http_url")
     confadcs['policyid'] = config.get("global", "policyid")
+    confadcs['path_cert'] = config.get("global", "path_cert")
+    confadcs['path_csr'] = config.get("global", "path_csr")
 
     with open(path_ca_cert, 'r') as f:
         confadcs['ca_certificate'] = (
