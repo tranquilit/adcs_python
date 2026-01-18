4eb5fa54d24aff230553af2bcbd6befbdcd11b07
Simon Fonteneau <sfonteneau@tranquil.it>
2026-01-18 14:46:51 +0100

    Integrate user search in authentication check

--- callbacks/auth_basic_template.py
+++ callbacks/auth_basic_template.py
@@ -1,10 +1,13 @@
-
+from utils import search_user
 
 def check_auth(username=None,password=None):
 
     #if auth ok return username
     #return username
 
+    r = search_user(userauth=username,password=password)
+    if r:
+        return r[1]['sAMAccountName'][0].decode('utf-8')
+    
     #if auth fail return False
     return False
-
