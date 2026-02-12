b913889419f1618449b8bf22bb14a42aa548e709
Simon Fonteneau <sfonteneau@tranquil.it>
2026-02-12 21:09:05 +0100

    Decode SID string to UTF-8 and update structure

--- callbacks/user_template.py
+++ callbacks/user_template.py
@@ -429,13 +429,13 @@ def emit_certificate(
     builder = _apply_static_extensions(builder, template)
 
     # ➕ dynamic NTDS (SID)
-    sid_str = samdbr.schema_format_value("objectSID", sam_entry["objectSID"][0])
+    sid_str = samdbr.schema_format_value("objectSID", sam_entry["objectSID"][0]).decode('utf-8')
     sid_bytes = sid_str.encode("ascii")
-
+    
     ntds_der = NtdsCASecurityExt({
         "other_name": {
             "type_id": "1.3.6.1.4.1.311.25.2.1",
-            "value": sid_bytes,
+            "value": {"value": sid_bytes}, 
         }
     }).dump()
 
