d7d5b01fe0c69fd3b1b8e935401ad1e5f3e17e8b
Simon Fonteneau <blog@lesfourmisduweb.org>
2026-06-14 10:58:35 +0200

    Update auth_basic_template.py

--- callbacks/auth_basic_template.py
+++ callbacks/auth_basic_template.py
@@ -1,4 +1,3 @@
-
 from utils import search_user
 
 def check_auth(username=None, password=None):
@@ -6,12 +5,8 @@ def check_auth(username=None, password=None):
     if not password:
         return False
 
-    # if auth ok return username
-    # return username
-
     r = search_user(userauth=username, password=password)
     if r:
-        return r[1]['sAMAccountName'][0].decode('utf-8')
+        return username
 
-    # if auth fail return False
     return False
